cb46807aa8b73f98c36825b9427dce11dc2cce25
Jonathan Bydendyk <jpbydendyk@gmail.com>
2021-06-29 15:39:28 +0200

    Added better method to handle project config

--- arnold/config.py
+++ arnold/config.py
@@ -1,3 +1,5 @@
+import os
+
 GPIO_MAP = {
     "motion": {
         "left": {
@@ -6,5 +8,16 @@ GPIO_MAP = {
         "right": {
             "pins": [17, 22]
         }
+    }
+}
+
+# Component config
+SENSOR_CONFIG = {
+    'microphone': {
+        'card_number': 1,
+        'device_number': 0
+    },
+    'google_cloud': {
+        "api_key": os.environ.get('ARNOLD_GOOGLE_CLOUD_APIKEY', '')
     }
 }
