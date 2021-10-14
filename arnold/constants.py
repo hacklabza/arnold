b886c7f324ed71e2fb77bce2c264fbb88c5d410e
Jonathan Bydendyk <jpbydendyk@gmail.com>
2021-10-14 19:57:37 +0200

    Moved constants to root project dir

---
+++ arnold/constants.py
@@ -0,0 +1,71 @@
+COMMAND_MAP = {
+    'motion': {
+        'tokens': ['go', 'proceed', 'move', 'travel', 'walk', 'drive'],
+        'map': {
+            'class': 'motion.drive.Drive',
+            'methods': [
+                {
+                    'tokens': ['forward', 'forth', 'frontwards', 'front'],
+                    'method': 'forward',
+                    'params': [
+                        {
+                            'tokens': ['seconds', 'steps'],
+                            'param': 'duration',
+                            'param_value': 'suffix'
+                        }
+                    ]
+                },
+                {
+                    'tokens': ['back', 'rear', 'backward', 'backwards', 'reverse'],
+                    'method': 'back',
+                    'params': [
+                        {
+                            'tokens': ['seconds', 'steps'],
+                            'param': 'duration',
+                            'param_value': 'suffix'
+                        }
+                    ]
+                },
+                {
+                    'tokens': ['turn', 'twist', 'swing'],
+                    'method': 'turn',
+                    'params': [
+                        {
+                            'tokens': ['right', 'rightward', 'rightwards'],
+                            'param': 'direction',
+                            'param_value': 'right'
+                        },
+                        {
+                            'tokens': ['left', 'leftward', 'leftwards'],
+                            'param': 'direction',
+                            'param_value': 'left'
+                        },
+                        {
+                            'tokens': ['seconds', 'steps'],
+                            'param': 'duration',
+                            'param_value': 'suffix'
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+    'lookup': {
+        'tokens': ['weather'],
+        'map': {
+            'class': 'lookup.weather.Weather',
+            'methods': [
+                {
+                    'tokens': ['today', 'now'],
+                    'method': 'today',
+                    'params': []
+                },
+                {
+                    'tokens': ['tomorrow'],
+                    'method': 'tomorrow',
+                    'params': []
+                }
+            ]
+        }
+    }
+}
