a99a0e6d5da5322da8722d3adf4e98bb4a591d8e
Jonathan Bydendyk <jonathan@hacklab.co.za>
2025-09-09 09:15:52 +0200

    Added drivetrain to init classes

--- arnold/main.py
+++ arnold/main.py
@@ -84,7 +84,7 @@ class Arnold(object):
         """
         Run Arnold in voice command mode.
         """
-        self._setup_classes(['microphone', 'openai', 'speaker'])
+        self._setup_classes(['microphone', 'openai', 'speaker', 'drivetrain'])
 
         # Capture the audio and parse the command or fall back to an OpenAI
         # response
