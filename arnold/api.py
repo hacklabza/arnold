03438588e4e74482c5727cf04caca3fd272557c8
Jonathan Bydendyk <jonathan@hacklab.co.za>
2025-09-30 18:34:34 +0200

    Attempt to mount api at api/ route

--- arnold/api.py
+++ arnold/api.py
@@ -10,22 +10,18 @@ from arnold import config, models
 API_CONFIG = config.API
 
 
-app = FastAPI(title='Arnold API', version='1.0.0')
+api = FastAPI(title='Arnold API', version='1.0.0')
 
 
-# Global arnold instance
-arnold_instance = None
-
-
-@app.get('/health')
+@api.get('/health')
 def health():
     return {'success': True}
 
 
-@app.post('/motion/drivetrain/go')
+@api.post('/motion/drivetrain/go')
 def drivetrain_go(request: models.DrivetrainRequest):
     try:
-        arnold_instance.drivetrain.go(
+        api.arnold.drivetrain.go(
             direction=request.direction,
             duration=request.duration,
             speed=request.speed
@@ -35,28 +31,28 @@ def drivetrain_go(request: models.DrivetrainRequest):
     return {'success': True}
 
 
-@app.post('/motion/drivetrain/stop')
+@api.post('/motion/drivetrain/stop')
 def drivetrain_stop():
     try:
-        arnold_instance.drivetrain.stop()
+        api.arnold.drivetrain.stop()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     return {'success': True}
 
 
-@app.post('/output/speaker/say')
+@api.post('/output/speaker/say')
 def speaker_say(request: models.SpeakerRequest):
     try:
-        arnold_instance.speaker.say(request.phrase)
+        api.arnold.speaker.say(request.phrase)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     return {'success': True}
 
 
-@app.get('/sensor/camera/stream')
+@api.get('/sensor/camera/stream')
 def camera_stream():
     return StreamingResponse(
-        arnold_instance.camera.stream_video(),
+        api.arnold.camera.stream_video(),
         media_type='multipart/x-mixed-replace; boundary=frame'
     )
 
@@ -68,8 +64,9 @@ def runserver(
     debug: Optional[bool] = None,
     reload: Optional[bool] = None
 ) -> None:
-    global arnold_instance
-
+    """
+    Run the FastAPI server with uvicorn.
+    """
     host = host or API_CONFIG['host']
     port = port or API_CONFIG['port']
     debug = debug or API_CONFIG['debug']
@@ -78,5 +75,11 @@ def runserver(
     # Set the global arnold instance for access in routes
     arnold_instance = arnold
 
+    # Initialize the FastAPI api with an instance of arnold
+    api.arnold = arnold_instance
+
+    # Mount the routes
+    api.mount("/api", api)
+
     # Start the FastAPI server with uvicorn
-    uvicorn.run(app, host=host, port=port, reload=reload)
+    uvicorn.run(api, host=host, port=port, reload=reload)
